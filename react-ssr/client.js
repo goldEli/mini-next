d3f6dee4cb654f3cf67a1dde58d25cd0a383591e
eli <eli107377@weexdev.com>
2024-11-09 19:51:00 +0800

    feat: hydrateRoot

--- react-ssr/client.js
+++ react-ssr/client.js
@@ -1,8 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './pages/index';
-import { createRoot } from 'react-dom/client';
+import { createRoot, hydrateRoot } from 'react-dom/client';
 
-const root = createRoot(document.getElementById('root'));
+// ssr 模式 会渲染两次
+// const root = createRoot(document.getElementById('root'));
 
-root.render(<App />);
+// root.render(<App />);
+
+// 使用 hydrateRoot 进行水合 会复用服务端渲染的 html
+hydrateRoot(document.getElementById('root'), <App />);
